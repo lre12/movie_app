4ddc3d87315aa5b41337264057da73645fccec73
lre12 <swj951105@naver.com>
2020-03-17 18:11:51 +0900

    #2.5

--- src/App.js
+++ src/App.js
@@ -1,46 +1,25 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Food({name, image}){
-  return <div>
-    <h1>I like {name} </h1>
-    <img src ={image} alt ={name}/>
-  </div>
-}
-
-const foodILike = [
-  {
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
-  },
-  {
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
-  },
-  {
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
-  },
-  {
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
-  }
-];
-
-function App() {
-  return  <div>
-    {foodILike.map(likeit => (
-      <Food name={likeit.name} image ={likeit.image}/>
-    ))}
-  </div>;
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  add = () => {
+    console.log("add");
+    this.state.count++;
+  };
+  minus = () => {
+    console.log("minus");
+    this.state.count--;
+  };
+  render(){
+   return <div>
+     <h1> The number is {this.state.count}</h1>
+     <button onClick={this.add}>Add</button>
+     <button onClick={this.minus}>minus</button>
+     </div>
+ }
 }
 
 export default App;
